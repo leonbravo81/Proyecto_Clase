0752b050255d7285b82f01a70790aac61bb63f0a
leonbravo81 <leonbravo81@misena.edu.co>
2022-06-30 18:49:36 -0500

    EntidadesMapeadas

--- skills/src/main/java/com/desarrollo/skills/entidades/TipoIdentificacion.java
+++ skills/src/main/java/com/desarrollo/skills/entidades/TipoIdentificacion.java
@@ -1,14 +1,27 @@
 package com.desarrollo.skills.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 //Clase de tipo entidad para guardar los tipos de identificacioon
 
 @Getter
 @Setter
+@Entity
+@Table(name="TBL_Tipo_Identificacion")
 public class TipoIdentificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    @Column (name="Nombre")
     private String Nombre;
+    @Column (name="Estado")
     private boolean Estado;
 
 }
