b9418ab9b9091d3fed8bcb4bc3016b2344f1a964
leonbravo81 <leonbravo81@misena.edu.co>
2022-06-29 20:19:54 -0500

    DiezVeinte

--- skills/src/main/java/com/desarrollo/skills/entidades/Profesiones.java
+++ skills/src/main/java/com/desarrollo/skills/entidades/Profesiones.java
@@ -1,12 +1,24 @@
 package com.desarrollo.skills.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 //Clase tipo entidad para guardar las profesiones de un profesional
 @Getter
 @Setter
+@Entity
+@Table (name="TBL_Profesiones")
 public class Profesiones {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    @Column (name="Nombres")
     private String Nombre;
+    @Column (name="Estado")
     private boolean Estado;
 }
--- skills/src/main/java/com/desarrollo/skills/entidades/SkillsProfesionales.java
+++ skills/src/main/java/com/desarrollo/skills/entidades/SkillsProfesionales.java
@@ -3,15 +3,30 @@ package com.desarrollo.skills.entidades;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 //Clase tipo entidad para guardar los skills de los profesionales
 
 @Getter
 @Setter
+@Entity
+@Table (name="TBL_Profesiones_Profesional")
 public class SkillsProfesionales {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column (name="Id_Profesion")
     private Long Id_Profesion;
+    @Column (name="Id_Profesional")
     private Long Id_Profesional;
-    private Date fecha_Graduacion;
+    @Column (name="fecha_Graduacion")
+    private Date Fecha_Graduacion;
     
 }
