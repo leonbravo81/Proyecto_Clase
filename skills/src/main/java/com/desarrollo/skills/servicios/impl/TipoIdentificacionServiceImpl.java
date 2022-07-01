1218ef5c7d626349bf9691b29bd7992e0d495cc8
leonbravo81 <leonbravo81@misena.edu.co>
2022-06-30 19:50:30 -0500

    Veinticinco_28Junio

--- skills/src/main/java/com/desarrollo/skills/servicios/impl/TipoIdentificacionServiceImpl.java
+++ skills/src/main/java/com/desarrollo/skills/servicios/impl/TipoIdentificacionServiceImpl.java
@@ -2,10 +2,15 @@ package com.desarrollo.skills.servicios.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.desarrollo.skills.dto.TipoIdentificacionDto;
 import com.desarrollo.skills.servicios.ITipoIdentificacionService;
 
 public class TipoIdentificacionServiceImpl implements ITipoIdentificacionService{
+
+    //Inyeccion de una interfaz que ya esta en memoria
+    @Autowired
 
     @Override
     public List<TipoIdentificacionDto> listarTiposIdentificacion() {
